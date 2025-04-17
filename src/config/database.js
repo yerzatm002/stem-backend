3b238fbd6b40b0272f9d8102f501e860dfc2d6c1
yerzatm002 <erzatnis@gmail.com>
2025-04-17 11:22:46 +0500

    fixing

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,14 @@ const config = require("./config.json")[process.env.NODE_ENV || "development"];
 const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
   dialect: config.dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
+;
 
 (async () => {
   try {
